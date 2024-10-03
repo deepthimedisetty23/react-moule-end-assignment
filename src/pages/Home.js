9ef4862009ab3851856a320322ce1f76c705fc7d
Deepthi gontla <deepthimedisetty23@gmail.com>
2024-10-03 16:10:23 +0530

    send

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ProductCard from '../components/ProductCard';
+import products from '../data';
 
-const products = [
-  { id: 1, name: 'Product 1', price: 29.99 },
-  { id: 2, name: 'Product 2', price: 39.99 },
-  { id: 3, name: 'Product 3', price: 49.99 },
-];
+// const products = [
+//   { id: 1, name: 'Product 1', price: 29.99 },
+//   { id: 2, name: 'Product 2', price: 39.99 },
+//   { id: 3, name: 'Product 3', price: 49.99 },
+// ];
 
 const Home = () => (
   <div>
